7dc933697065c290d04c885a28d4f47f5010a68f
srivani <srivani@graduate.utm.my>
2022-02-04 22:25:25 +0800

    profile seller created

--- lib/screens/ProfileSeller/profileseller_viewmodel.dart
+++ lib/screens/ProfileSeller/profileseller_viewmodel.dart
@@ -3,18 +3,17 @@ import '../../services/ProfileSeller/profileseller_service.dart';
 import '../../models/profileseller.dart';
 import '../viewmodel.dart';
 
-
 class ProfileSellerMainViewmodel extends Viewmodel {
   final _service = locator<ProfileSellerService>();
   List<ProfileSeller> _list;
 
-  ProfileSeller getProfileSeller(int index) => _list == null ? null : _list[index];
+  ProfileSeller getProfileSeller(int index) =>
+      _list == null ? null : _list[index];
   int get dataCount => _list == null ? 0 : _list.length;
 
   int _index;
   int get editIndex => _index;
   set editIndex(value) => _index = value;
-
 
   @override
   init() => update(() async {
@@ -25,10 +24,10 @@ class ProfileSellerMainViewmodel extends Viewmodel {
   void editProfileSeller(int index) => update(() async {
         _index = index;
       });
-  void updateProfileSeller({dynamic id, ProfileSeller data}) => update(() async {
+  void updateProfileSeller({dynamic id, ProfileSeller data}) =>
+      update(() async {
         final index = _list.indexWhere((note) => note.id == id);
         if (index == -1) return null;
         _list[index] = data.copyWith(id: id);
       });
-
 }
